58e015bb5ad27b08296ca2025f6bf8bc23259410
Carlos <ce4985028@gmail.com>
2026-02-26 13:51:37 -0300

    Estudo de arrays

---
+++ aulasJS_Arrays/script.js
@@ -0,0 +1,16 @@
+const listaDeCompras = ["Arroz", "Feijão", "Ovos"];
+
+listaDeCompras[3] = "Farofa";
+
+listaDeCompras.push("Maçã"); //automaticamente coloca na última posição do array
+
+const convidados = ["Sheila", "Cleyton", "Letícia"];
+
+convidados.push("André");
+
+convidados.length
+
+convidados[convidados.length -1]; //seleciona o último convidado da lista
+
+convidados[convidados.length] = "Paulo"; //adiciona "Paulo" na última posição da lista, sem excluir nada
+
