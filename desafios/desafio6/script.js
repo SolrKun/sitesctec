adb95a1c011104557b529cff3d2db95de4777070
Carlos <ce4985028@gmail.com>
2026-02-26 13:56:46 -0300

    Desenvolvendo o desafio 6

---
+++ desafios/desafio6/script.js
@@ -0,0 +1,14 @@
+const frutas = ["Abacaxi", "Maçã", "Melancia", "Pera", "Morango"];
+
+console.log(frutas);
+
+console.log(frutas[2]);
+
+frutas[2] = "Banana";
+
+console.log(frutas[2]);
+
+frutas.push("mamão");
+frutas.push("laranja");
+
+console.log(frutas);
