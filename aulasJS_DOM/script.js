dc4903ece2b477dcadd162ed743841940a25ee44
Carlos Silva <ce4985028@gmail.com>
2026-02-27 17:57:36 -0300

    Criação de um jogo da velha

--- aulasJS_DOM/script.js
+++ aulasJS_DOM/script.js
@@ -17,3 +17,17 @@ function retornaCor() {
     div2.style.backgroundColor = "";
     div3.style.backgroundColor = "";
 }
+
+/* Seletores
+
+getElementById = recupera o elemento pelo ID
+getElementsByTagName = recupera vários elementos pela tag
+getElementsByClassName = recupera vários elementos pela classe
+querySelector = recupera o primeiro elemento correspondente a um seletor CSS
+querySelectorAll = recupera todos os elementos correspondentes a um seletor CSS
+createElement = cria um novo elemento do DOM
+appendChild = acrescenta um novo elemento filho a um elemento existente
+removeChild = remove um elemento filho de um elemento existente
+setAttribute = define o valor de um atributo
+addEventListener = adiciona um ouvinte de eventos a um evento
+*/
