a336ce87ff4f0da7c7473c8b694a8d20ecfec9f3
Carlos Silva <ce4985028@gmail.com>
2026-02-28 19:49:25 -0300

    Desenvolvimento do desafio 13

---
+++ desafios/desafio13/app.js
@@ -0,0 +1,6 @@
+var calculadora = require("./commons/calculadora");
+
+console.log(calculadora.soma(1, 2));
+console.log(calculadora.subtracao(5,2));
+console.log(calculadora.multiplicacao(2,2));
+console.log(calculadora.divisao(2,2));
